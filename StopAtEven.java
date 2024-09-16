51a96e6d6c6ff736bfca9589c923208b75d0eba5
Tanushree N Naik <136624898+TanushreeN@users.noreply.github.com>
2024-09-16 22:56:01 +0530

    Add files via upload

---
+++ StopAtEven.java
@@ -0,0 +1,23 @@
+//Question : The user gives input number, till even number it will receives input and if odd njumber is entered, stop taking next input and display the total number of inputs taken.
+
+import java.util.Scanner;
+
+public class StopAtEven
+{
+	public static void main(String[] args)
+	{
+		int n = 0, a, i,count = 0;
+		System.out.println("Enter the numbers");	
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+				
+		while(n%2 == 0)
+		{
+			//System.out.println(n);
+			n = sc.nextInt();
+			count++;
+		}
+		//int c = n.length();	
+		System.out.println("The total numbers enters are:"+ (count+1));
+	}
+}
